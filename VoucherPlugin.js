a94b89e8e727bcfd2ee060c856ce992efe61568d
zkgit <iszk@foxmail.com>
2018-11-01 09:41:41 +0800

    抢券

--- VoucherPlugin.js
+++ VoucherPlugin.js
@@ -1,17 +1,18 @@
-
-function S_FAll(e){
+function S_FAll(e) {
     return document.querySelectorAll(e)
 }
-function S_F(e){
+
+function S_F(e) {
     return document.querySelector(e)
 }
+
 function transformers(res) {
     var e = document.createEvent("MouseEvents");
     e.initEvent("click", true, true);
     if (S_FAll(res)) {
         var len = S_FAll(res).length
         setInterval(function () {
-            for (var i = len-1; i >= 1; i--) {
+            for (var i = len - 1; i >= 1; i--) {
                 (function (i) {
                     console.info(i, S_FAll(res)[i])
                     if (S_FAll(res)[i]) {
